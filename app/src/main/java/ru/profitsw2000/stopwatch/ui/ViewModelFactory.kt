8cd006396c96ac0cf52d7a1b4306d027348e2d5d
profitsw2000 <profitsw2000@gmail.com>
2022-06-17 19:50:44 +0300

    Добавлен класс ViewModelFactory.kt для создания экземпляров класса MainViewModel.

---
+++ app/src/main/java/ru/profitsw2000/stopwatch/ui/ViewModelFactory.kt
@@ -0,0 +1,11 @@
+package ru.profitsw2000.stopwatch.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.profitsw2000.stopwatch.data.RepositoryImpl
+
+class ViewModelFactory (private val repository: RepositoryImpl) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(repository) as T
+    }
+}
